261bfce387f156ba3a8f30e01beae593d3b13870
Jason M <jbird206@gmail.com>
2020-01-13 16:56:07 -0800

    Add files via upload

---
+++ calculator/JS/scripts.js
@@ -0,0 +1,48 @@
+// var add = function(number1, number2) {
+//     return number1 + number2;
+//   };
+
+//BMI calculator
+// var BMI = function(number1, number2, number3) {
+//     return (number1 * number2) / (number3 * number3);
+// };
+
+//   var number1 = parseInt(prompt("Weight"));
+//   var number2 = parseInt("703");
+//   var number3 = parseInt(prompt("height"));
+//   var result = BMI(number1, number2, number3);
+
+
+//   alert(result);
+// end BMI calculator//////////
+
+//Celsius to Farenheit conversion
+
+function temperature(number1, number2, number3, number4) {
+    return (number1 * number2) * number3 + number4;
+};
+
+
+// var number1 = parseInt(prompt("Celcius temperature"));
+// var number2 = parseInt("2");
+// var number3 = parseFloat(".1");
+// var number4 = parseInt("32");
+// var result = temperature(number1, number2, number3, number4);
+// alert(result);
+
+//prompt("Celcius temp");
+var number1 = parseInt(prompt("Celcius temperature"));
+var number2 = 2;
+var number3 = 0.1;
+var number4 = 32;
+var result = temperature(number1, number2, number3, number4);
+alert(result);
+
+
+
+
+
+
+
+
+  
